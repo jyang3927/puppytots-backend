f362787121cdb130af67c96eee8179fd24c79d22
Jamie Yang <yangjm1287@gmail.com>
2024-05-16 14:33:15 -0400

    changed to cloud functions for storage

---
+++ functions/src/config/firebase.config.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv'; 
+import { initializeApp } from 'firebase-admin';
+
+dotenv.config(); 
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
+
+const app = initializeApp(firebaseConfig); 
+
+
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,8 +4,13 @@ import cors from 'cors';
 import dogsRouter from './routes/dogsRouter';
 import puppiesRouter from './routes/puppiesRouter';
 
+import * as dotenv from 'dotenv'
+dotenv.config(); 
+
 const app = express(); 
 app.use(cors()); 
+app.use(express.json()); 
+
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
     res.status(500).json({ message: "Internal Server Error" });
@@ -13,7 +18,8 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
 
 app.use(errorHandler); 
 
+
 app.use("/api", puppiesRouter); 
 app.use("/api", dogsRouter); 
 
-export const backendAPI = functions.https.onRequest(app); 
+export const backendAPI = functions.https.onRequest(app);
